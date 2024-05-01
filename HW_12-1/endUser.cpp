78c9024e8bc9289a95d8e456267f99db217ca8bb
bkelleher81 <brock.kelleher@gmail.com>
2024-05-01 16:15:13 -0500

    #

--- HW_12-1/endUser.cpp
+++ HW_12-1/endUser.cpp
@@ -5,16 +5,17 @@ using namespace std;
 int main() 
 {
     char str[100]; // Assuming max length of input string is 100 characters
+    WordCounter h;
 
     // Prompt user for input
     cout << "Enter a string: ";
     cin.getline(str, 100);
 
     // Call the function to count words
-    int count = WordCounter::countWords(str);
+    int count = h.countWords(str);
 
     // Display the result
-    std::cout << "Number of words: " << count << std::endl;
+    cout << "Number of words: " << count << std::endl;
 
     return 0;
 }
